bd59ffd8be0e1bf2338732dce7a28a107b5a0918
Ian Be <ian84be@gmail.com>
2019-07-07 15:43:52 -0500

    scaffold auth functions, add/remove users

--- server/api/server.js
+++ server/api/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-// const restricted = require('../auth/restricted-middleware.js');
-
-// const authRouter = require('../auth/auth-router');
-// const usersRouter = require('../api/users-router');
+const restricted = require('./restricted-middleware.js');
+const authRouter = require('./auth-router');
+const usersRouter = require('./users-router'); 
 // const tripsRouter = require('../api/trips-router');
 // const profileRouter = require('../api/profile-router');
 
@@ -14,8 +13,8 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', restricted, usersRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/users', restricted, usersRouter);
 // server.use('/api/trips', restricted, tripsRouter);
 // server.use('/api/profile', restricted, profileRouter);
 
