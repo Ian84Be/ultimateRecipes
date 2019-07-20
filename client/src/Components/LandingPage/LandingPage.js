b47fca12fdeb2b5ca77dd30c8e8dc2d40c015869
Ian Be <ian84be@gmail.com>
2019-07-20 12:25:59 -0500

    add Formik, Yup, refactor LandingPage

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-// import {NavLink} from 'react-router-dom'
+import axios from 'axios';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 import {
     Button, 
     Divider, 
@@ -8,13 +10,14 @@ import {
     Header, 
     Icon, 
     Input, 
+    Message,
     Modal,
     Segment} from 'semantic-ui-react'
 
 const LandingPage = (props) => {
     const [myUsername, setMyUsername] = useState('');
     const [myPassword, setMyPassword] = useState('');
-
+    const [errMsg, setErrMsg] = useState('');
     return ( 
         <>
         <Header as='h2' icon textAlign='center'>
@@ -28,25 +31,61 @@ const LandingPage = (props) => {
 
             <Grid.Row verticalAlign='middle'>
                 <Grid.Column>
-                <Header>
-                    Login
-                </Header>
+                    <Header>
+                        Login
+                    </Header>
 
-                <Form onSubmit={(e) => props.userLogin(e, myUsername, myPassword)}>
-                    <Form.Field 
-                        control={Input} 
-                        label="Username"
-                        onChange={(e) => setMyUsername(e.target.value)}
-                        required
+                    <Formik 
+                        initialValues={{
+                            username: '',
+                            password: ''
+                        }}
+                        onSubmit={(values, actions) => {
+                            actions.setSubmitting(true);
+                            axios.post('/auth/login', values)
+                            .then(res => {
+                                actions.resetForm();
+                                actions.setSubmitting(false);
+                                localStorage.setItem('token', res.data.token);
+                                localStorage.setItem('username', res.data.username);
+                            })
+                            // .then(()=> getShopList())
+                            .catch(err => {
+                                actions.setSubmitting(false);
+                                console.log(JSON.stringify(err));
+                                setErrMsg(err.message);
+                            });
+                        }}
+                        validationSchema={() => Yup.object().shape({
+                            username: Yup.string().max(10, 'username must be 10 characters or less').required('please enter a username!'),
+                            password: Yup.string().required('please enter a password!'),
+                        })}
+                        render={props => (
+                            <Form error onSubmit={props.handleSubmit}>
+                                <Form.Field 
+                                    control={Input} 
+                                    error={props.touched.username && props.errors.username && { content: props.errors.username, pointing: 'below' }}
+                                    label="Username"
+                                    name="username"
+                                    onChange={props.handleChange}
+                                    value={props.values.username}
+                                />
+                                <Form.Field 
+                                    control={Input} 
+                                    error={props.touched.password && props.errors.password && { content: props.errors.password, pointing: 'below' }}
+                                    label="Password"
+                                    name="password"
+                                    onChange={props.handleChange}
+                                    value={props.values.password}
+                                />
+                                <Button 
+                                    loading={props.isSubmitting} 
+                                    primary color='black' 
+                                    type='submit'>Login</Button>
+                                {errMsg && <Message error header='Error' content={errMsg}/>}
+                            </Form>
+                        )}
                     />
-                    <Form.Field 
-                        control={Input} 
-                        label="Password"
-                        onChange={(e) => setMyPassword(e.target.value)}
-                        required
-                    />
-                    <Button primary color='black' type='submit'>Login</Button>
-                </Form>
                 </Grid.Column>
 
                 <Grid.Column>
